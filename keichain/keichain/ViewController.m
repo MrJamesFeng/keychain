918416bfc268e2894192d8ffcc9f7b45f377ba8b
MrJamesFeng <812463918@qq.co>
2017-04-28 10:35:43 +0800

    基本完成

--- keichain/keichain/ViewController.m
+++ keichain/keichain/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <Security/Security.h>
 @interface ViewController ()
 
 @end
@@ -17,13 +17,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
+-(void)add{
+    
+}
+-(void)delete{
+    
+}
+-(void)update{
+    
+}
+-(void)search{
+    
+}
+-(void)share{
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
 @end
---
+++ keichain/keichain/libs/JSONKit/QHJSONKit.m
@@ -0,0 +1,170 @@
+//
+//  QHJSONKit.m
+//  Foundation
+//
+//  Created by Keldon on 16-6-7.
+//  Copyright (c) 2014年 Keldon. All rights reserved.
+//
+
+#import "QHJSONKit.h"
+
+@implementation QHJSONKit
+
+@end
+
+@implementation NSString(JSONCategories)
+
++(NSString *)stringFromJSONData:(NSData *)jsonData
+{
+    
+    __autoreleasing NSError* error = nil;
+    id result = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
+    if (error != nil) return nil;
+    return result;
+    
+}
++(NSString *)stringFromJSONData:(NSData *)jsonData options:(NSJSONReadingOptions)opt error:(NSError **)error
+{
+    id result = [NSJSONSerialization JSONObjectWithData:jsonData options:opt error:error];
+    if (error != nil) return nil;
+    return result;
+}
+
+-(NSData *)toJSON
+{
+    if ([NSJSONSerialization isValidJSONObject:self]) {
+        NSError* error = nil;
+        id result = [NSJSONSerialization dataWithJSONObject:self options:kNilOptions error:&error];
+        if (error != nil) return nil;
+        return result;
+    }else{
+        return [self dataUsingEncoding:NSUTF8StringEncoding];
+    }
+    
+}
+
+-(NSData *)toJSONWithoptions:(NSJSONWritingOptions)opt error:(NSError **)error
+{
+    if ([NSJSONSerialization isValidJSONObject:self]) {
+        id result = [NSJSONSerialization dataWithJSONObject:self options:opt error:error];
+        if (error != nil) return nil;
+        return result;
+    }else{
+        return nil;
+    }
+}
+
+-(NSArray *)arrayFromJSONString
+{
+    NSData* data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    
+    return  [NSArray arrayFromJSONData:data];
+}
+
+-(NSDictionary *)dictionaryFromJSONString
+{
+    NSData* data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    
+    return  [NSDictionary dictionaryFromJSONData:data];
+}
+
+@end
+
+@implementation NSArray(JSONCategories)
+
++(NSArray *)arrayFromJSONData:(NSData *)jsonData
+{
+    __autoreleasing NSError* error = nil;
+    id result = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
+    if (error != nil) return nil;
+    return result;
+}
++(NSArray *)arrayFromJSONData:(NSData *)jsonData options:(NSJSONReadingOptions)opt error:(NSError **)error
+{
+    id result = [NSJSONSerialization JSONObjectWithData:jsonData options:opt error:error];
+    if (error != nil) return nil;
+    return result;
+}
+
+-(NSData *)toJSON
+{
+    if ([NSJSONSerialization isValidJSONObject:self]) {
+        NSError* error = nil;
+        id result = [NSJSONSerialization dataWithJSONObject:self options:kNilOptions error:&error];
+        if (error != nil) return nil;
+        return result;
+    }else{
+        return nil;//-.[NSKeyedArchiver archivedDataWithRootObject:self];
+    }
+}
+
+-(NSData *)toJSONWithoptions:(NSJSONWritingOptions)opt error:(NSError **)error
+{
+    if ([NSJSONSerialization isValidJSONObject:self]) {
+        id result = [NSJSONSerialization dataWithJSONObject:self options:opt error:error];
+        if (error != nil) return nil;
+        return result;
+    }else{
+        return nil;
+    }
+}
+
+-(NSString *)JSONString
+{
+    NSData* data = [self toJSON];
+    return [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+}
+
+@end
+
+@implementation NSDictionary(JSONCategories)
+
++(NSDictionary *)dictionaryFromJSONData:(NSData *)jsonData
+{
+    if (jsonData==nil) {
+        return nil;
+    }
+    __autoreleasing NSError* error = nil;
+    id result = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
+    if (error != nil) return nil;
+    return result;
+}
+
++(NSDictionary *)dictionaryFromJSONData:(NSData *)jsonData options:(NSJSONReadingOptions)opt error:(NSError **)error
+{
+    id result = [NSJSONSerialization JSONObjectWithData:jsonData options:opt error:error];
+    if (error != nil) return nil;
+    return result;
+}
+
+-(NSData *)toJSON
+{
+    if ([NSJSONSerialization isValidJSONObject:self]) {
+        NSError* error = nil;
+        id result = [NSJSONSerialization dataWithJSONObject:self options:kNilOptions error:&error];
+        if (error != nil) return nil;
+        return result;
+    }else{
+        return nil;//[NSKeyedArchiver archivedDataWithRootObject:self];
+    }
+}
+
+-(NSData *)toJSONWithoptions:(NSJSONWritingOptions)opt error:(NSError **)error
+{
+    if ([NSJSONSerialization isValidJSONObject:self]) {
+        id result = [NSJSONSerialization dataWithJSONObject:self options:opt error:error];
+        if (error != nil) return nil;
+        return result;
+    }else{
+        return nil;
+    }
+}
+
+-(NSString *)JSONString
+{
+    NSData* data = [self toJSON];
+    return [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+}
+
+@end
+
